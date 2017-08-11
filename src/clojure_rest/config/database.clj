60a6eca3f58c9f7093ee85ee33634ccfc9b11e4b
m.sauboua <sauboua.beneluz.matthieu@gmail.com>
2017-08-11 17:51:37 +0200

    [BUILD BROKEN] Currently changing jdbc usage and versions

--- src/clojure_rest/config/database.clj
+++ src/clojure_rest/config/database.clj
@@ -2,30 +2,15 @@
   (:import com.mchange.v2.c3p0.ComboPooledDataSource)
   (:require [clojure.java.jdbc :as sql]))
 
-(def db-config
+(def db
       {:classname "org.h2.Driver"
        :subprotocol "h2"
        :subname "mem:documents"
-       :user ""
+       :user "sa"
        :password ""})
 
-(defn pool
-     [config]
-     (let [cpds (doto (ComboPooledDataSource.)
-                  (.setDriverClass (:classname config))
-                  (.setJdbcUrl (str "jdbc:" (:subprotocol config) ":" (:subname config)))
-                  (.setUser (:user config))
-                  (.setPassword (:password config))
-                  (.setMaxPoolSize 6)
-                  (.setMinPoolSize 1)
-                  (.setInitialPoolSize 1))]
-       {:datasource cpds}))
-
-(def pooled-db (delay (pool db-config)))
-
-(defn db-connection [] @pooled-db)
-
-(sql/with-connection (db-connection)
-      (sql/create-table :documents [:id "varchar(256)" "primary key"]
-                                   [:title "varchar(1024)"]
-                                   [:text :varchar]))
+(sql/db-do-commands db
+      (sql/create-table-ddl :documents
+                            [:id "integer" "primary key"]
+                            [:title "varchar(1024)"]
+                            [:text :varchar]))
