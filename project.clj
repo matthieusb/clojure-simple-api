24f06c086948e74cea78bef8d04fb1008c61c2c4
m.sauboua <sauboua.beneluz.matthieu@gmail.com>
2017-08-15 12:00:04 +0200

    Removed useless mongodb support

--- project.clj
+++ project.clj
@@ -8,7 +8,6 @@
                  [cheshire "5.7.1"] ; Json parsing librairy
                  [org.clojure/java.jdbc "0.7.0"] ; jdbc support
                  [com.h2database/h2 "1.4.196"] ; h2 database support
-                 [com.novemberain/monger "3.1.0"] ; mongodb support
                  [ring/ring-json "0.4.0"]
                  [ring/ring-defaults "0.3.1"]] ; ring server default dependencies
   :plugins [[lein-ring "0.12.0"]
