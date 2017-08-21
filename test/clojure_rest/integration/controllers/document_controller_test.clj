7e82512c05f00f45528dc875763951f47572a073
Matthieu Sauboua-Beneluz <sauboua.beneluz.matthieu@gmail.com>
2017-08-21 22:46:06 +0200

    Added some rest tests to fix

--- test/clojure_rest/integration/controllers/document_controller_test.clj
+++ test/clojure_rest/integration/controllers/document_controller_test.clj
@@ -6,7 +6,39 @@
 
 (deftest test-document-controller
   (database/initDatabase)
-  (testing "Testing document list route"
+
+  (testing "Testing document list route worked"
     (let [response (app (mock/request :get "/documents"))]
+      (is (= (:status response) 200))))
+
+  (testing "Testing get document by id route worked"
+    (let [response (app (mock/request :get "/documents"))]
+      (is (= (:status response) 200))))
+
+  (testing "Testing get document by id route failed with incorrect id"
+    (let [response (app (mock/request :get "/documents/1"))]
+      (is (= (:status response) 200))))
+
+  (testing "Testing document creation route worked"
+    (let [response (app (mock/request :post "/documents"))]
+      (is (= (:status response) 200))))
+
+  (testing "Testing document creation route failed"
+    (let [response (app (mock/request :post "/documents"))]
+      (is (= (:status response) 200))))
+
+  (testing "Testing document update route worked"
+    (let [response (app (mock/request :put "/documents/1"))]
+      (is (= (:status response) 200))))
+
+  (testing "Testing document update route failed"
+    (let [response (app (mock/request :put "/documents/1"))]
+      (is (= (:status response) 200))))
+
+  (testing "Testing document delete route worked"
+    (let [response (app (mock/request :delete "/documents/1"))]
+      (is (= (:status response) 200))))
+
+  (testing "Testing document delete route failed"
+    (let [response (app (mock/request :delete "/documents/1"))]
       (is (= (:status response) 200)))))
-      ;(is (= (:body response) "Not found"))))
