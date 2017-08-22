b50a12a32bb758b414c01072a20942ef2bc491f0
Matthieu Sauboua-Beneluz <sauboua.beneluz.matthieu@gmail.com>
2017-08-22 14:05:39 +0200

    Adding some more tests on creation

--- src/clojure_rest/config/database.clj
+++ src/clojure_rest/config/database.clj
@@ -4,14 +4,20 @@
             [clojure-rest.config.app :as appconf]
             [clojure-rest.utils.database-utils :as dbutils]))
 
+(declare dropAllTables)
+(declare createDocumentsTable)
+(declare addSomeTestDocuments)
+
 (def db-h2-connection
   (get appconf/conf :h2database))
 
-; (def db-h2-connection
-;      {:dbtype "h2"
-;       :dbname "./resources/main/db/clojure_rest_h2"
-;       :user "sa"
-;       :password ""})
+(defn initDatabase
+  "Calls methods to intialize database at application startup and shows them at the end"
+  []
+  (log/info "Initializing whole database")
+  (dropAllTables)
+  (createDocumentsTable)
+  (addSomeTestDocuments))
 
 (defn dropAllTables
   "Drops all tables before recreating them"
@@ -26,10 +32,9 @@
   (dbutils/createTable db-h2-connection :documents [[:id_document "varchar(36)" "PRIMARY KEY"]
                                                     [:title "varchar(32)"]
                                                     [:text "varchar(64)"]]))
-
-(defn initDatabase
-  "Calls methods to intialize database at application startup and shows them at the end"
+(defn addSomeTestDocuments
+  "Just adds some dummy documents for test purposes"
   []
-  (log/info "Initializing whole database")
-  (dropAllTables)
-  (createDocumentsTable))
+  (log/info "Adding some dummy documents")
+  (jdbc/insert! db-h2-connection :documents {:id_document "1" :title "titre1" :text "text1"})
+  (jdbc/insert! db-h2-connection :documents {:id_document "2" :title "titre2" :text "text2"}))
--- test/clojure_rest/integration/controllers/document_controller_test.clj
+++ test/clojure_rest/integration/controllers/document_controller_test.clj
@@ -1,44 +1,67 @@
 (ns clojure-rest.integration.controllers.document-controller-test
   (:require [clojure.test :refer :all]
             [ring.mock.request :as mock]
+            [cheshire.core :as json]
             [clojure-rest.handler :refer :all]
             [clojure-rest.config.database :as database]))
 
-(deftest test-document-controller
+(def expectedHeaders {"Content-Type" "application/json; charset=utf-8"})
+(def initialDocumentList (json/generate-string [{:id_document "1" :title "titre1" :text "text1"} {:id_document "2" :title "titre2" :text "text2"}]))
+
+(deftest test-document-controller-list
   (database/initDatabase)
 
   (testing "Testing document list route worked"
     (let [response (app (mock/request :get "/documents"))]
-      (is (= (:status response) 200))))
+      (is (= (:status response) 200))
+      (is (= (:headers response) expectedHeaders))
+      (is (= (:body response) initialDocumentList)))))
 
+(deftest test-document-controller-getbyid
+  (database/initDatabase)
   (testing "Testing get document by id route worked"
-    (let [response (app (mock/request :get "/documents"))]
-      (is (= (:status response) 200))))
-
-  (testing "Testing get document by id route failed with incorrect id"
     (let [response (app (mock/request :get "/documents/1"))]
       (is (= (:status response) 200))))
 
+  (testing "Testing get document by id route failed with incorrect id"
+    (let [response (app (mock/request :get "/documents/notfound"))]
+      (is (= (:status response) 404)))))
+
+(deftest test-document-controller-create
+  (database/initDatabase)
+
   (testing "Testing document creation route worked"
-    (let [response (app (mock/request :post "/documents"))]
-      (is (= (:status response) 200))))
-
-  (testing "Testing document creation route failed"
-    (let [response (app (mock/request :post "/documents"))]
-      (is (= (:status response) 200))))
-
-  (testing "Testing document update route worked"
-    (let [response (app (mock/request :put "/documents/1"))]
-      (is (= (:status response) 200))))
-
-  (testing "Testing document update route failed"
-    (let [response (app (mock/request :put "/documents/1"))]
-      (is (= (:status response) 200))))
-
-  (testing "Testing document delete route worked"
-    (let [response (app (mock/request :delete "/documents/1"))]
-      (is (= (:status response) 200))))
-
-  (testing "Testing document delete route failed"
-    (let [response (app (mock/request :delete "/documents/1"))]
+    (let [request (mock/request :post "/documents" (json/generate-string {:id_document "dummy" :title "newDocumentTitle" :text "newDocumentText"}))]
+    (let [response (app (mock/content-type request "application/json"))]
       (is (= (:status response) 200)))))
+    ) ; TODO Add body test and count test
+
+  ; TODO
+  ; (testing "Testing document creation route failed"
+  ;   (let [response (app (mock/request :post "/documents"))]
+  ;     (is (= (:status response) 200))))
+
+
+
+; (deftest test-document-controller-update
+;   (database/initDatabase)
+;
+;   (testing "Testing document update route worked"
+;     (let [response (app (mock/request :put "/documents/1"))]
+;       (is (= (:status response) 200))))
+;
+;   (testing "Testing document update route failed"
+;     (let [response (app (mock/request :put "/documents/1"))]
+;       (is (= (:status response) 200)))))
+
+
+; (deftest test-document-controller-delete
+;   (database/initDatabase)
+;
+;   (testing "Testing document delete route worked"
+;     (let [response (app (mock/request :delete "/documents/1"))]
+;       (is (= (:status response) 200))))
+;
+;   (testing "Testing document delete route failed"
+;     (let [response (app (mock/request :delete "/documents/1"))]
+;       (is (= (:status response) 200)))))
