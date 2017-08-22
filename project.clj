a4c8ae29a497404b278bacc55f36913f2c30c93c
Matthieu Sauboua-Beneluz <sauboua.beneluz.matthieu@gmail.com>
2017-08-22 16:51:06 +0200

    Updated cheshire version

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "https://github.com/matthieusb/clojure-simple-api"
   :min-lein-version "2.0.0"
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"] ; Clojure version
                  [org.clojure/tools.logging "0.4.0"]
-                 [compojure "1.6.0"]
-                 [cheshire "5.7.1"] ; Json parsing librairy
+                 [compojure "1.6.0"] ; routing library
+                 [cheshire "5.8.0"] ; Json parsing librairy
                  [org.clojure/java.jdbc "0.7.0"] ; jdbc support
                  [com.h2database/h2 "1.4.196"] ; h2 database support
-                 [ring/ring-json "0.4.0"]
+                 [ring/ring-json "0.4.0"] ; ring server json support
                  [ring/ring-defaults "0.3.1"]] ; ring server default dependencies
 
   :plugins [[lein-ring "0.12.0"]
