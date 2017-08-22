3f0c118c5512f9a168edd6c5525ebfb2c562c8dc
Matthieu Sauboua-Beneluz <sauboua.beneluz.matthieu@gmail.com>
2017-08-22 16:46:22 +0200

    Finushed writing the tests, but some are red

--- test/clojure_rest/integration/controllers/document_controller_test.clj
+++ test/clojure_rest/integration/controllers/document_controller_test.clj
@@ -2,6 +2,7 @@
   (:require [clojure.test :refer :all]
             [ring.mock.request :as mock]
             [cheshire.core :as json]
+            [clojure.java.jdbc :as jdbc]
             [clojure-rest.handler :refer :all]
             [clojure-rest.config.database :as database]))
 
@@ -34,34 +35,37 @@
     (let [request (mock/request :post "/documents" (json/generate-string {:id_document "dummy" :title "newDocumentTitle" :text "newDocumentText"}))]
     (let [response (app (mock/content-type request "application/json"))]
       (is (= (:status response) 200)))))
-    ) ; TODO Add body test and count test
+  ; TODO Add body test and count test
 
-  ; TODO
-  ; (testing "Testing document creation route failed"
-  ;   (let [response (app (mock/request :post "/documents"))]
-  ;     (is (= (:status response) 200))))
+  (testing "Testing document creation route failed because of incorrect document"
+    (let [request (mock/request :post "/documents" (json/generate-string {:id_document "dummy" :title "newDocumentTitle" :text "newDocumentText" :wrongattribute "badvalue"}))]
+    (let [response (app (mock/content-type request "application/json"))]
+      (is (= (:status response) 400)))))) ; FIXME This test causes an error
 
+(deftest test-document-controller-update
+  (database/initDatabase)
 
+  (testing "Testing document update route worked"
+    (let [request (mock/request :put "/documents/1" (json/generate-string {:id_document "1" :title "titre1Updated" :text "texte1Updated"}))]
+      (let [response (app (mock/content-type request "application/json"))]
+      (is (= (:status response) 200)))))
+  ; TODO Add tests on body returned and database
 
-; (deftest test-document-controller-update
-;   (database/initDatabase)
-;
-;   (testing "Testing document update route worked"
-;     (let [response (app (mock/request :put "/documents/1"))]
-;       (is (= (:status response) 200))))
-;
-;   (testing "Testing document update route failed"
-;     (let [response (app (mock/request :put "/documents/1"))]
-;       (is (= (:status response) 200)))))
+  (testing "Testing document update route failed with incorrect id"
+    (let [request (mock/request :put "/documents/notfound" (json/generate-string {:id_document "noid" :title "wontwork" :text "causeIncorrectid"}))]
+      (let [response (app (mock/content-type request "application/json"))]
+      (is (= (:status response) 404)))))
+  ) ; TODO Add tests on body returned and database
 
+(deftest test-document-controller-delete
+  (database/initDatabase)
 
-; (deftest test-document-controller-delete
-;   (database/initDatabase)
-;
-;   (testing "Testing document delete route worked"
-;     (let [response (app (mock/request :delete "/documents/1"))]
-;       (is (= (:status response) 200))))
-;
-;   (testing "Testing document delete route failed"
-;     (let [response (app (mock/request :delete "/documents/1"))]
-;       (is (= (:status response) 200)))))
+  (testing "Testing document delete route worked"
+    (let [response (app (mock/request :delete "/documents/1"))]
+      (is (= (:status response) 204))))
+  ; TODO Add tests on database
+
+  (testing "Testing document delete route failed"
+    (let [response (app (mock/request :delete "/documents/notfound"))]
+      (is (= (:status response) 404))))) ; FIXME This test fails
+; TODO Add tests on database and correct error
