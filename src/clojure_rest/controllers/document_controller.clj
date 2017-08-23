7c494eb830a3c2e984085538908e6645813f85cb
Matthieu Sauboua-Beneluz <sauboua.beneluz.matthieu@gmail.com>
2017-08-23 18:02:53 +0200

    Added a service for jdbc operations, dao might follow

--- src/clojure_rest/controllers/document_controller.clj
+++ src/clojure_rest/controllers/document_controller.clj
@@ -6,6 +6,7 @@
             [clojure.java.jdbc :as jdbc]
             [clojure.tools.logging :as log]
             [ring.middleware.json :as ring-json]
+            [clojure-rest.services.document-service :as documentService]
             [clojure-rest.model.document :as document]
             [clojure-rest.config.database :as database]))
 
@@ -14,11 +15,11 @@
 (defn get-all-documents []
   (log/info "get-all-documents")
   (response
-      (jdbc/query database/db-h2-connection ["select * from documents"])))
+      (documentService/getAllDocuments)))
 
 (defn get-document [id]
   (log/info (str "get-document with id : " id))
-  (def results (jdbc/query database/db-h2-connection ["select * from documents where id_document = ?" id] {}))
+  (def results (documentService/getDocumentBydId id))
     (cond (empty? results) {:status 404}
       :else (response (first results))))
 
@@ -29,7 +30,7 @@
     (let [id (uuid)]
       (log/info (str "create-new-document generated id : " id))
       (let [document (assoc doc :id_document id)]
-        (jdbc/insert! database/db-h2-connection :documents document))
+        (documentService/createNewDocument document))
         (get-document id)))))
 
 (defn update-document [id doc]
@@ -37,11 +38,11 @@
   (let [validDocument (document/validate-document-map doc)]
     (if (nil? validDocument) {:status 400}
       (let [document (assoc doc :id_document id)]
-        (jdbc/update! database/db-h2-connection :documents document ["id_document=?" id])
+        (documentService/updateDocument id document)
     (get-document id)))))
 
 (defn delete-document [id]
   (log/info (str "delete-document with id : " id))
-  (let [numberOfRowsDeletedArray (jdbc/delete! database/db-h2-connection :documents ["id_document=?" id])]
+  (let [numberOfRowsDeletedArray (documentService/deleteDocument id)]
     (println (str "number of rows deleted : " numberOfRowsDeletedArray))
     (if (> (first numberOfRowsDeletedArray) 0) {:status 204} {:status 404})))
---
+++ src/clojure_rest/services/document_service.clj
@@ -0,0 +1,30 @@
+(ns clojure-rest.services.document-service
+  (:require [clojure.java.jdbc :as jdbc]
+            [clojure.tools.logging :as log]
+            [clojure-rest.model.document :as document]
+            [clojure-rest.config.database :as database]))
+
+(defn getAllDocuments
+  "Gets all documents from the database"
+  []
+  (jdbc/query database/db-h2-connection ["select * from documents"]))
+
+(defn getDocumentBydId
+  "Gets only one document using its id"
+  [idDocument]
+  (jdbc/query database/db-h2-connection ["select * from documents where id_document = ?" idDocument] {}))
+
+(defn createNewDocument
+  "Adds a new document to the database"
+  [documentToCreate]
+  (jdbc/insert! database/db-h2-connection :documents documentToCreate))
+
+(defn updateDocument
+  "Updates an existing document on the database"
+  [idDocumentToUpdate documentToUpdate]
+  (jdbc/update! database/db-h2-connection :documents documentToUpdate ["id_document=?" idDocumentToUpdate]))
+
+(defn deleteDocument
+  "Deletes an existing document on the database, returns number of deleted rows"
+  [idDocumentToDelete]
+  (jdbc/delete! database/db-h2-connection :documents ["id_document=?" idDocumentToDelete]))
