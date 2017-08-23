ed879a57aa7f55278a0f11205041af36e5d96aac
Matthieu Sauboua-Beneluz <sauboua.beneluz.matthieu@gmail.com>
2017-08-23 15:07:14 +0200

    Another attempt at coveralls

--- test/coveralls.sh
+++ test/coveralls.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
 COVERALLS_URL='https://coveralls.io/api/v1/jobs'
-CLOVERAGE_VERSION='1.0.4-SNAPSHOT' lein cloverage -o test/cov --coveralls
+CLOVERAGE_VERSION='1.0.9'
+lein cloverage -o test/cov --coveralls
 curl -F 'json_file=@test/cov/coveralls.json' "$COVERALLS_URL"
